021eb589d6a8c3899ad3a568a88328af4c65beff
dragenkit <marvinauner47@gmail.com>
2024-07-15 16:48:48 +0200

    fixed function getComputerChoise

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ const choicePaper = "Paper"
 const choiceSissors = "Sissors"
 
 function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 2)
+  let randomNumber = Math.floor(Math.random() * 3)
 
 if (randomNumber === 0) {
   return choiceRock
 } else if (randomNumber === 1) {
   return choicePaper
-} else {
+} else if (randomNumber === 2) {
   return choiceSissors
 }
 }
-
-
-
-
 
