ca9710984fd3daddeb35ea6ce1d32cd901bf5890
dragenkit <marvinauner47@gmail.com>
2024-07-25 08:00:44 +0200

    started the function playround

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ if (randomNumber === 0) {
 }
 }
 
-function getHumanChoise() {
+function getHumanChoice() {
  let humanChoise = prompt("Rock,Paper,Sissor");
 
 humanChoise.toLocaleLowerCase();
@@ -24,3 +24,31 @@ return humanChoise
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound() {
+
+getComputerChoice()
+
+getHumanChoise()
+
+  if (getComputerChoice() = choiceRock) {
+    if (getHumanChoice() = choiceRock) {
+      console.log("draw")
+      console.log("current score" + "human: " + humanScore + "computer: " + computerScore)
+    }
+  }
+  if (getComputerChoice() = choiceRock) {
+    if (getHumanChoice() = choicePaper) {
+      humanScore + 1;
+      console.log("you win");
+      console.log("current score" + "human: " + humanScore + "computer: " + computerScore)
+    }
+  }
+  if (getComputerChoice() = choiceRock) {
+    if(getHumanChoice() = choiceSissors) {
+      console.log("you lose")
+      console.log("current score" + "human: " + humanScore + "computer: " + computerScore)
+    }
+  }
+
+
+}
